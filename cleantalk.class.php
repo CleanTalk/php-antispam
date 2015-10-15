16cc4657c6527c25c33afb13d2f3ac2db3e76b5f
VladCleantalk <v.gorbatov@bk.ru>
2015-10-15 19:43:53 +0500

    fix for phpbb3.1

--- cleantalk.class.php
+++ cleantalk.class.php
@@ -2,7 +2,7 @@
 /**
  * Cleantalk base class
  *
- * @version 2.1.1
+ * @version 2.1.2
  * @package Cleantalk
  * @subpackage Base
  * @author Cleantalk team (welcome@cleantalk.org)
@@ -1144,6 +1144,11 @@ if( !function_exists('apache_request_headers') )
 	{
 		$arh = array();
 		$rx_http = '/\AHTTP_/';
+		if(defined('IN_PHPBB'))
+		{
+			global $request;
+			$request->enable_super_globals();
+		}
 		foreach($_SERVER as $key => $val)
 		{
 			if( preg_match($rx_http, $key) )
@@ -1159,18 +1164,29 @@ if( !function_exists('apache_request_headers') )
 				$arh[$arh_key] = $val;
 			}
 		}
+		if(defined('IN_PHPBB'))
+		{
+			global $request;
+			$request->disable_super_globals();
+		}
 		return( $arh );
 	}
 }
 
 function cleantalk_get_real_ip()
 {
+	if(defined('IN_PHPBB'))
+	{
+		global $request;
+		$request->enable_super_globals();
+	}
 	if ( function_exists( 'apache_request_headers' ) )
 	{
 		$headers = apache_request_headers();
 	}
 	else
 	{
+		
 		$headers = $_SERVER;
 	}
 	if ( array_key_exists( 'X-Forwarded-For', $headers ) )
@@ -1186,6 +1202,11 @@ function cleantalk_get_real_ip()
 	else
 	{
 		$the_ip = filter_var( $_SERVER['REMOTE_ADDR'], FILTER_VALIDATE_IP, FILTER_FLAG_IPV4 );
+	}
+	if(defined('IN_PHPBB'))
+	{
+		global $request;
+		$request->disable_super_globals();
 	}
 	return $the_ip;
 }
