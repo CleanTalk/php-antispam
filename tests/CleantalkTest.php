bc696346f870787949ed444918909589598f24b6
davydovct <davydov@cleantalk.org>
2018-09-16 19:49:59 +0500

    setting test key

--- tests/CleantalkTest.php
+++ tests/CleantalkTest.php
@@ -7,7 +7,7 @@ use lib\Cleantalk;
 use lib\CleantalkRequest;
 use lib\CleantalkResponse;
 
-define ('CLEANTALK_TEST_API_KEY', 'CLEANTALK_TEST_API_KEY');
+define ('CLEANTALK_TEST_API_KEY', 'ejujapepugu2');
 
 class CleantalkTest extends \PHPUnit\Framework\TestCase 
 {
@@ -26,25 +26,25 @@ class CleantalkTest extends \PHPUnit\Framework\TestCase
 	public function testIsAllowMessage()
 	{
 		$this->ct_request->sender_email = 'good@mail.org';
-		$this->ct_request->sender_message = 'good message';
+		$this->ct_request->message = 'good message';
 		$result = $this->ct->isAllowMessage($this->ct_request);
 		$this->assertEquals(1, $result->allow);
 		$this->assertEquals(0, $result->stop_queue);
 
 		$this->ct_request->sender_email = 'good@mail.org';
-		$this->ct_request->sender_message = 'stop_word bad message';
+		$this->ct_request->message = 'stop_word bad message';
 		$result = $this->ct->isAllowMessage($this->ct_request);
 		$this->assertEquals(0, $result->allow);	
 		$this->assertEquals(0, $result->stop_queue);
 
 		$this->ct_request->sender_email = 's@cleantalk.org';
-		$this->ct_request->sender_message = 'good message';
+		$this->ct_request->message = 'good message';
 		$result = $this->ct->isAllowMessage($this->ct_request);
 		$this->assertEquals(0, $result->allow);	
 		$this->assertEquals(1, $result->stop_queue);
 
 		$this->ct_request->sender_email = 's@cleantalk.org';
-		$this->ct_request->sender_message = 'stop_word bad message';
+		$this->ct_request->message = 'stop_word bad message';
 		$result = $this->ct->isAllowMessage($this->ct_request);
 		$this->assertEquals(0, $result->allow);		
 		$this->assertEquals(1, $result->stop_queue);			
