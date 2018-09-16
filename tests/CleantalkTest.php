4841e457caa982b188928ebdff39234fdabaefb7
davydovct <davydov@cleantalk.org>
2018-09-17 03:52:11 +0500

    update test

--- tests/CleantalkTest.php
+++ tests/CleantalkTest.php
@@ -29,25 +29,11 @@ class CleantalkTest extends \PHPUnit\Framework\TestCase
 		$this->ct_request->message = 'good message';
 		$result = $this->ct->isAllowMessage($this->ct_request);
 		$this->assertEquals(1, $result->allow);
-		$this->assertEquals(0, $result->stop_queue);
-
-		$this->ct_request->sender_email = 'good@mail.org';
-		$this->ct_request->message = 'stop_word bad message';
-		$result = $this->ct->isAllowMessage($this->ct_request);
-		$this->assertEquals(0, $result->allow);	
-		$this->assertEquals(0, $result->stop_queue);
-
-		$this->ct_request->sender_email = 's@cleantalk.org';
-		$this->ct_request->message = 'good message';
-		$result = $this->ct->isAllowMessage($this->ct_request);
-		$this->assertEquals(0, $result->allow);	
-		$this->assertEquals(1, $result->stop_queue);
 
 		$this->ct_request->sender_email = 's@cleantalk.org';
 		$this->ct_request->message = 'stop_word bad message';
 		$result = $this->ct->isAllowMessage($this->ct_request);
-		$this->assertEquals(0, $result->allow);		
-		$this->assertEquals(1, $result->stop_queue);			
+		$this->assertEquals(0, $result->allow);					
 
 		$this->ct_request->message = '';
 		$this->ct_request->sender_email = '';
