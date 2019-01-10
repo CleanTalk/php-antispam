085fc232eb0d764e925688d26b239dd4ee2100e8
Артем <artem@ip-192-168-0-101.is74.loc>
2019-01-10 12:20:30 +0500

    update tests require

--- tests/CleantalkTest.php
+++ tests/CleantalkTest.php
@@ -1,6 +1,10 @@
 <?php
 require_once "lib/Cleantalk.php";
 require_once "lib/CleantalkRequest.php";
+require_once "lib/CleantalkResponse.php";
+require_once "lib/CleantalkHelper.php";
+require_once "lib/CleantalkAPI.php";
+require_once "lib/cleantalk-php-patch.php";
 
 use lib\Cleantalk;
 use lib\CleantalkRequest;
