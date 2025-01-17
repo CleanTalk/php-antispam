19a173113b2c20ec88e007753d1c7fb3f38561a3
Glomberg <bazz@bk.ru>
2025-01-17 14:37:17 +0300

    Fix. Tests. Psalm notice fixed.

--- cleantalk-antispam.php
+++ cleantalk-antispam.php
@@ -5,4 +5,4 @@ require_once "lib/CleantalkHelper.php";
 require_once "lib/CleantalkRequest.php";
 require_once "lib/CleantalkResponse.php";
 require_once "lib/CleantalkAntispam.php";
-require_once "lib/CleantalkApi.php";
+require_once "lib/CleantalkAPI.php";
